3a06fb6ba5be4a26c8214a95c69cd6536410d4d5
mferrer-almirall <41777999+mferrer-almirall@users.noreply.github.com>
2020-01-16 08:26:54 -0100

    Update venn_updown.R

--- venn_updown.R
+++ venn_updown.R
@@ -57,7 +57,7 @@ createVennEuler <- function(topTabs, compNames, label = "selected", colFeat = "X
                                   main = titulo,
                                   filename = NULL,
                                   rotation.degree = rotation,
-                                  cat.pos = position)
+                                  cat.pos = position, margin=0.2)
         if (pltPdf) {
             pdf(paste0("VennDiagram", include, ".",label, ".", colPVal, pval, ".logFC",FC,".pdf"))
             grid.draw(venn.plot)
